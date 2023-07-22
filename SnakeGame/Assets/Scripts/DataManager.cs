7010a59fea6f9033333de54d7e774ae7eadb7075
Omrron <obaron4120@gmail.com>
2023-07-22 15:54:43 +0300

    Added data manager and instance data

---
+++ SnakeGame/Assets/Scripts/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class DataManager
+    {
+        private static readonly string m_SaveDir = Application.persistentDataPath + "/SaveData";
+        private const string m_SaveFile = "/InstanceData.json";
+
+        public static SaveData LoadData()
+        {
+            string fullSavePath = m_SaveDir + m_SaveFile;
+
+            if (File.Exists(fullSavePath))
+            {
+                string json = File.ReadAllText(fullSavePath);
+                return JsonUtility.FromJson<SaveData>(json);
+            }
+            else
+            {
+                return new SaveData()
+                {
+                    SpeedMultiplier = 1f,
+                    HighScore = 0,
+                    IsFullScreen = false
+                };
+            }
+        }
+
+        public static void SaveData(SaveData data)
+        {
+            string json = JsonUtility.ToJson(data, true);
+            StringBuilder pathMaker = new StringBuilder(m_SaveDir);
+            pathMaker.Append("/InstanceData.json");
+
+            if (!Directory.Exists(m_SaveDir))
+            {
+                Directory.CreateDirectory(m_SaveDir);
+            }
+
+            File.WriteAllText(pathMaker.ToString(), json);
+        }
+    }
+}
---
+++ SnakeGame/Assets/Scripts/InstanceData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class InstanceData : MonoBehaviour
+    {
+        public SaveData Data;
+        public static InstanceData Instance;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+
+                return;
+            }
+
+            Data = DataManager.LoadData();
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void OnApplicationQuit()
+        {
+            DataManager.SaveData(Data);
+        }
+    }
+}

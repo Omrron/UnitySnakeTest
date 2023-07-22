7613fe1920b57382342f415e16ba51f725072d60
Omrron <obaron4120@gmail.com>
2023-07-22 15:55:28 +0300

    Removed debugger code

--- SnakeGame/Assets/Scripts/SceneSwitchButtonScript.cs
+++ SnakeGame/Assets/Scripts/SceneSwitchButtonScript.cs
@@ -11,10 +11,5 @@ public class SceneSwitchButtonScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-
-        if (UnityEditor.EditorApplication.isPlaying)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
     }
 }
